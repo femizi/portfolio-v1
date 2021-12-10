2a6794199b61e8fb3690a80ad714ea4790795060
Femi Efejuku <obito.femi@gmail.com>
2021-12-10 17:43:52 +0100

    dark mode

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// dark-mode handling
+const toggleBtn = document.querySelector(".top-tooltip")
+const clickDay = document.querySelector('.click-day')
+const clickNight = document.querySelector('.click-night')
+function changeImg(){
+  clickDay.classList.toggle("hidden")
+  clickNight.classList.toggle("hidden")
+  }
+function toggle (){
+  document.body.classList.toggle("dark-mode");
+  changeImg()
+  
+
+}
+
+toggleBtn.addEventListener("click", toggle)
+
+
+
 // modal handling
 const modal = document.querySelector(".modal");
 const loginBtn = document.querySelector(".hamburger");
